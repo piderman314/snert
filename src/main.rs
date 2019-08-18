00474d8705f0ba235c6233088549b945994f54f4
Mark Arts <piderman+github@gmail.com>
2019-08-18 17:40:50 +0200

    Add some logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+mod cartridge;
+
+use std::env;
+use std::process;
+
+use log::info;
+use log::LevelFilter;
+
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::Appender;
+use log4rs::config::Config;
+use log4rs::config::Root;
+use log4rs::encode::pattern::PatternEncoder;
+
 fn main() {
-    println!("Hello, world!");
+    setup_logging();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Need the ROM filename as argument");
+        process::exit(1);
+    }
+
+    let rom_file = &args[1];
+    info!("Using rom file {}", rom_file);
+}
+
+fn setup_logging() {
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "{d(%F %T%.3f)} {l} {M} \\{{T}\\} {m}{n}",
+        )))
+        .build();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Debug))
+        .unwrap();
+    log4rs::init_config(config).unwrap();
 }
