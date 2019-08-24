5c074f2edeb9a82d23e59c51161bdd016716e0f4
Mark Arts <piderman+github@gmail.com>
2019-08-24 23:14:33 +0200

    Add simple logic for reset vector

--- src/cartridge/info/mod.rs
+++ src/cartridge/info/mod.rs
@@ -1,15 +1,27 @@
 mod game_title;
 
+use crate::mem::MemAddr;
 use game_title::GameTitle;
 
-use crate::cartridge::Cartridge;
+use crate::cartridge::{CartAddr, Cartridge};
 
 pub struct Info<'cartridge> {
     pub game_title: GameTitle<'cartridge>,
+    pub reset_vector: MemAddr,
 }
 
 pub fn from_cartridge(cartridge: &Cartridge) -> Info {
     Info {
         game_title: game_title::from_cartridge(cartridge),
+        reset_vector: reset_vecor(cartridge),
     }
+}
+
+const RESET_VECTOR_OFFSET: usize = 0x7FFC;
+
+fn reset_vecor(cartridge: &Cartridge) -> MemAddr {
+    let rv = cartridge[CartAddr(RESET_VECTOR_OFFSET)] as usize
+        + ((cartridge[CartAddr(RESET_VECTOR_OFFSET + 1)] as usize) << 8);
+
+    MemAddr(rv)
 }
